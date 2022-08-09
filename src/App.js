68d29834463927db3fc1479b3b472240c7c8e447
MD Abdul Zabber Islam Apurbo <md.abdul.zabber.islam.apurbo@g.bracu.ac.bd>
2022-08-09 21:18:34 +0600

    router added but still bug is there

--- src/App.js
+++ src/App.js
@@ -1,63 +1,84 @@
 // import logo from "./logo.svg";
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
-// import About from "./components/About";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  const[alert, setAlert] = useState(null);
-  const[mode, setMode] = useState("light");
-  const[mystyle1, setMyStyle1] = useState({
-    display : "block"
+  const [alert, setAlert] = useState(null);
+  const [mode, setMode] = useState("light");
+  const [mystyle1, setMyStyle1] = useState({
+    display: "block",
   });
-  const[mystyle2, setMyStyle2] = useState({
-    display : "none"
+  const [mystyle2, setMyStyle2] = useState({
+    display: "none",
   });
   const showAlert = (message, type) => {
     setAlert({
-      msg : message,
-      type : type
+      msg: message,
+      type: type,
     });
     setTimeout(() => {
       setAlert(null);
     }, 1500);
-  }
+  };
   const togglemode1 = () => {
-    if(mode === "light"){
+    if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "black";
       setMyStyle1({
-        display : "none"
+        display: "none",
       });
       setMyStyle2({
-        display : "block"
+        display: "block",
       });
       showAlert("dark mode has been enabled", "success");
     }
-  }
+  };
   const togglemode2 = () => {
-    if(mode === "dark"){
+    if (mode === "dark") {
       setMode("light");
       document.body.style.backgroundColor = "white";
       setMyStyle2({
-        display : "none"
+        display: "none",
       });
       setMyStyle1({
-        display : "block"
+        display: "block",
       });
       showAlert("light mode has been enabled", "success");
     }
-  }
+  };
   return (
     <>
-      <Navbar title="TextUtils" about="About Us" modes={mode} togglemode1={togglemode1} togglemode2={togglemode2} mystyle1={mystyle1} mystyle2={mystyle2} />
-      <Alert alert={alert}/>
-      <div className="container">
-        <TextForm heading="Enter your text" modes={mode} showAlert={showAlert} />
-        {/* <About/> */}
-      </div>
+      <Router>
+        <Navbar
+          title="TextUtils"
+          about="About Us"
+          modes={mode}
+          togglemode1={togglemode1}
+          togglemode2={togglemode2}
+          mystyle1={mystyle1}
+          mystyle2={mystyle2}
+        />
+        <Alert alert={alert} />
+        <div className="container">
+          <Switch>
+            <Route path="/">
+              <TextForm
+                heading="Enter your text"
+                modes={mode}
+                showAlert={showAlert}
+              />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
