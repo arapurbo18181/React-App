6325cee284025668dcb9157ef76706c5a19cae0e
MD Abdul Zabber Islam Apurbo <md.abdul.zabber.islam.apurbo@g.bracu.ac.bd>
2022-08-09 11:16:17 +0600

    jsx and props learned

---
+++ src/module1.mjs
@@ -0,0 +1,5 @@
+import asd, {c,b,d} from './module2.mjs';
+console.log(asd);
+console.log(b);
+console.log(d);
+console.log(c);
---
+++ src/module2.mjs
@@ -0,0 +1,9 @@
+const a = "apurbo";
+const b = "rakib";
+const c = "jowel";
+const d = "rifat";
+
+export default a;
+export {b};
+export {c};
+export {d};
